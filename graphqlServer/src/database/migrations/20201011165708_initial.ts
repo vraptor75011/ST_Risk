3b84189d42aa007e34e709a9ee231a3dbb3c474a
vraptor75011 <vraptor.coder@outlook.com>
2020-10-16 09:43:53 +0200

    end of test sqlmancer

--- graphqlServer/src/database/migrations/20201011165708_initial.ts
+++ graphqlServer/src/database/migrations/20201011165708_initial.ts
@@ -3,17 +3,17 @@ import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<any> {
     return knex.schema.createTableIfNotExists('users',(table:Knex.CreateTableBuilder)=>{
-        table.increments('UserId');
-        table.string('FullName',36);
-        table.integer('CountryCode');
+        table.increments('id');
+        table.string('full_name',36);
+        table.integer('country_code');
         table.timestamps(true,true);
 
     })
     .createTableIfNotExists('pets',(table:Knex.CreateTableBuilder)=>{
-        table.increments('PetId');
-        table.string('Name');
-        table.integer('OwnerId').references("users.UserId").onDelete("CASCADE");
-        table.string('Specie');
+        table.increments('id');
+        table.string('name');
+        table.integer('owner_id').references("users.id").onDelete("CASCADE");
+        table.string('specie');
         table.timestamps(true,true);
     })
 }
