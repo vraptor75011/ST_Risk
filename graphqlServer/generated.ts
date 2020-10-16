65ed891279f15b0b7c11e063fe812928a6cef3fc
vraptor75011 <vraptor.coder@outlook.com>
2020-10-16 09:44:14 +0200

    End of test sqlmancer

---
+++ graphqlServer/generated.ts
@@ -0,0 +1,222 @@
+import Knex from 'knex';
+import {
+  CreateManyBuilder,
+  CreateOneBuilder,
+  DeleteByIdBuilder,
+  DeleteManyBuilder,
+  FindByIdBuilder,
+  FindManyBuilder,
+  FindOneBuilder,
+  PaginateBuilder,
+  UpdateByIdBuilder,
+  UpdateManyBuilder
+} from 'sqlmancer';
+
+export type ID = number | string;
+export type JSON = boolean | number | string | null | JSONArray | JSONObject;
+export interface JSONObject {
+  [key: string]: JSON;
+}
+export type JSONArray = Array<JSON>;
+
+export type UserFields = {
+  id: number;
+  full_name: string;
+  country_code: string;
+  created_at: string;
+}
+    
+export type UserIds = unknown;
+    
+export type UserEnums = unknown;
+    
+export type UserAssociations = {
+  pets: [PetFindManyBuilder, PetPaginateBuilder];
+}
+    
+export type UserCreateFields = {
+  id?: number;
+  full_name?: string;
+  country_code?: string;
+  created_at?: string;
+};
+    
+export type UserUpdateFields = {
+  full_name?: string;
+  country_code?: string;
+  created_at?: string;
+};
+    
+export type UserFindOneBuilder<TSelected extends Pick<UserFields, any> = UserFields> = FindOneBuilder<
+  'postgres',
+  UserFields,
+  UserIds,
+  UserEnums,
+  UserAssociations,
+  TSelected
+>
+
+export type UserFindManyBuilder<TSelected extends Pick<UserFields, any> = UserFields> = FindManyBuilder<
+  'postgres',
+  UserFields,
+  UserIds,
+  UserEnums,
+  UserAssociations,
+  TSelected
+>
+
+export type UserFindByIdBuilder<TSelected extends Pick<UserFields, any> = UserFields> = FindByIdBuilder<
+  UserFields,
+  UserIds,
+  UserEnums,
+  UserAssociations,
+  TSelected
+>
+
+export type UserPaginateBuilder = PaginateBuilder<'postgres', UserFields, UserIds, UserEnums, UserAssociations>
+      
+export type UserDeleteManyBuilder = DeleteManyBuilder<
+  'postgres',
+  UserFields,
+  UserIds,
+  UserEnums,
+  UserAssociations
+>
+
+export type UserDeleteByIdBuilder = DeleteByIdBuilder
+
+export type UserCreateManyBuilder = CreateManyBuilder<UserCreateFields>
+
+export type UserCreateOneBuilder = CreateOneBuilder<UserCreateFields>
+
+export type UserUpdateManyBuilder = UpdateManyBuilder<
+  'postgres',
+  UserUpdateFields,
+  UserFields,
+  UserIds,
+  UserEnums,
+  UserAssociations
+>
+
+export type UserUpdateByIdBuilder = UpdateByIdBuilder<UserUpdateFields>
+  
+export type PetFields = {
+  id: number;
+  name: string;
+  owner_id: number;
+  specie: Species;
+  created_at: string;
+}
+    
+export type PetIds = unknown;
+    
+export type PetEnums = Species;
+    
+export type PetAssociations = {
+  owner: [UserFindOneBuilder, UserPaginateBuilder];
+}
+    
+export type PetCreateFields = {
+  id: number;
+  name?: string;
+  owner_id: number;
+  specie?: Species;
+  created_at?: string;
+};
+    
+export type PetUpdateFields = {
+  name?: string;
+  owner_id?: number;
+  specie?: Species;
+  created_at?: string;
+};
+    
+export type PetFindOneBuilder<TSelected extends Pick<PetFields, any> = PetFields> = FindOneBuilder<
+  'postgres',
+  PetFields,
+  PetIds,
+  PetEnums,
+  PetAssociations,
+  TSelected
+>
+
+export type PetFindManyBuilder<TSelected extends Pick<PetFields, any> = PetFields> = FindManyBuilder<
+  'postgres',
+  PetFields,
+  PetIds,
+  PetEnums,
+  PetAssociations,
+  TSelected
+>
+
+export type PetFindByIdBuilder<TSelected extends Pick<PetFields, any> = PetFields> = FindByIdBuilder<
+  PetFields,
+  PetIds,
+  PetEnums,
+  PetAssociations,
+  TSelected
+>
+
+export type PetPaginateBuilder = PaginateBuilder<'postgres', PetFields, PetIds, PetEnums, PetAssociations>
+      
+export type PetDeleteManyBuilder = DeleteManyBuilder<
+  'postgres',
+  PetFields,
+  PetIds,
+  PetEnums,
+  PetAssociations
+>
+
+export type PetDeleteByIdBuilder = DeleteByIdBuilder
+
+export type PetCreateManyBuilder = CreateManyBuilder<PetCreateFields>
+
+export type PetCreateOneBuilder = CreateOneBuilder<PetCreateFields>
+
+export type PetUpdateManyBuilder = UpdateManyBuilder<
+  'postgres',
+  PetUpdateFields,
+  PetFields,
+  PetIds,
+  PetEnums,
+  PetAssociations
+>
+
+export type PetUpdateByIdBuilder = UpdateByIdBuilder<PetUpdateFields>
+  
+export enum Species {
+  BIRDS = "BIRDS",
+  FISH = "FISH",
+  MAMMALS = "MAMMALS",
+  REPTILES = "REPTILES",
+}
+
+export type SqlmancerClient = Knex & {
+  models: {
+    User: {
+      findById: (id: ID) => UserFindByIdBuilder;
+      findMany: () => UserFindManyBuilder;
+      findOne: () => UserFindOneBuilder;
+      paginate: () => UserPaginateBuilder;
+      createMany: (input: UserCreateFields[]) => UserCreateManyBuilder;
+      createOne: (input: UserCreateFields) => UserCreateOneBuilder;
+      deleteById: (id: ID) => UserDeleteByIdBuilder;
+      deleteMany: () => UserDeleteManyBuilder;
+      updateById: (id: ID, input: UserUpdateFields) => UserUpdateByIdBuilder;
+      updateMany: (input: UserUpdateFields) => UserUpdateManyBuilder;
+    };
+    Pet: {
+      findById: (id: ID) => PetFindByIdBuilder;
+      findMany: () => PetFindManyBuilder;
+      findOne: () => PetFindOneBuilder;
+      paginate: () => PetPaginateBuilder;
+      createMany: (input: PetCreateFields[]) => PetCreateManyBuilder;
+      createOne: (input: PetCreateFields) => PetCreateOneBuilder;
+      deleteById: (id: ID) => PetDeleteByIdBuilder;
+      deleteMany: () => PetDeleteManyBuilder;
+      updateById: (id: ID, input: PetUpdateFields) => PetUpdateByIdBuilder;
+      updateMany: (input: PetUpdateFields) => PetUpdateManyBuilder;
+    };
+  };
+};
+  
--- graphqlServer/src/schema/index.ts
+++ graphqlServer/src/schema/index.ts
@@ -6,31 +6,56 @@ import { SqlmancerClient } from './graphql/generated';
 import typeDefs from './graphql/schema.graphql';
 
 const db = Knex(dbconfig["development"]);
-
-const client = createSqlmancerClient<SqlmancerClient>('/Users/vraptor/WebDev/ST_Risk/graphqlServer/src/schema/graphql/schema.graphql', db);
+console.log(__dirname);
+//const client = createSqlmancerClient<SqlmancerClient>('/Users/vraptor/WebDev/ST_Risk/graphqlServer/src/schema/graphql/schema.graphql', db);
+const client = createSqlmancerClient<SqlmancerClient>("./**/*.graphql", db);
 
 const { models: { User, Pet } } = client;
 
-const resolvers:IResolvers = {
+const resolvers: IResolvers = {
     Query: {
         users: (root, args, ctx, info) => {
             return User.findMany()
-            .resolveInfo(info)
-            .execute();
+                .resolveInfo(info)
+                .execute();
         },
         user: (_root, args, _ctx, info) => {
             return User.findById(args.id).resolveInfo(info).execute()
         },
         pets: (root, args, ctx, info) => {
             return Pet.findMany()
-            .resolveInfo(info)
-            .execute();
+                .resolveInfo(info)
+                .execute();
         },
         pet: (_root, args, _ctx, info) => {
             return Pet.findById(args.id).resolveInfo(info).execute()
         },
+    },
+    Mutation: {
+        createUser: async (root, args, ctx, info) => {
+            const id = await User.createOne(args.input).execute();
+            return User.findById(id).resolveInfo(info).execute()
+        },
+        createPet: async (root, args, ctx, info) => {
+            const id = await Pet.createOne(args.input).execute()
+            return Pet.findById(id).resolveInfo(info).execute()
+        },
+        deleteUser: (_root, args) => {
+            return User.deleteById(args.id).execute()
+        },
+        deletePet: (_root, args) => {
+            return Pet.deleteById(args.id).execute()
+        },
+        updateUser: async (_root, args, _ctx, info) => {
+            await User.updateById(args.id, args.input).execute()
+            return User.findById(args.id).resolveInfo(info).execute()
+        },
+        updatePet: async (_root, args, _ctx, info) => {
+            await Pet.updateById(args.id, args.input).execute()
+            return Pet.findById(args.id).resolveInfo(info).execute()
+        }
     }
-};
+}
   
 const schema = makeSqlmancerSchema({ typeDefs, resolvers: resolvers as any });
 
