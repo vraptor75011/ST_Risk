d456b6c07962ed1b90eaf209aaed3389625a0460
vraptor75011 <vraptor.coder@outlook.com>
2020-11-17 16:21:32 +0100

    .gitignore update

--- graphqlServer/jest.config.ts
+++ graphqlServer/jest.config.ts
@@ -17,18 +17,18 @@ export default {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["./src/**/*.ts"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+   coveragePathIgnorePatterns: [
+     "/node_modules/", "/src/jest/","src/@types/"
+   ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
