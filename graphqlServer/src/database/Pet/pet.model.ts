e1b22d05166aa0011ee45bd384e9a544e5c19088
vraptor75011 <vraptor.coder@outlook.com>
2020-11-26 12:06:38 +0100

    fix git

--- graphqlServer/src/database/Pet/pet.model.ts
+++ graphqlServer/src/database/Pet/pet.model.ts
@@ -1,30 +1,38 @@
-import { Field, ID, ObjectType } from "type-graphql";
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ID, ObjectType } from 'type-graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { User } from '../user/user.model';
 import { Species } from './pet.enum';
 
 @ObjectType()
 @Entity('pets')
 class Pet {
-    @Field(type => ID)
-    @PrimaryGeneratedColumn("increment")
-    id: string;
+  @Field((type) => ID)
+  @PrimaryGeneratedColumn('increment')
+  id: string;
 
-    @Field()
-    @Column({type: "varchar", length: 60, nullable: false})
-    name: string;
+  @Field()
+  @Column({ type: 'varchar', length: 60, nullable: false })
+  name: string;
 
-    @Field(type => Species)
-    @Column()
-    specie: Species;
+  @Field((type) => Species)
+  @Column()
+  specie: Species;
 
-    @Field()
-    @CreateDateColumn()
-    created_at: Date
+  @Field()
+  @CreateDateColumn()
+  created_at: Date;
 
-    @Field(type => User )
-    @ManyToOne(type => User, user => user.pets)
-    owner?: User;
+  @Field((type) => User)
+  @ManyToOne((type) => User, (user) => user.pets)
+  owner?: User;
 }
+
+//
 
 export { Pet };
--- graphqlServer/src/database/User/user.model.ts
+++ graphqlServer/src/database/User/user.model.ts
@@ -33,4 +33,6 @@ class User {
   pets?: Pet[];
 }
 
+//
+
 export { User };
